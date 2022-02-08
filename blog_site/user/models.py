8f2f87395f6d9d65b5b85cf84088b3844e2209c4
23egih23 <egor.filatov@live.com>
2022-02-08 17:02:22 +0300

    08.02_17:02

--- blog_site/user/models.py
+++ blog_site/user/models.py
@@ -3,4 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    #profession
+    #about
+    #work_experience
+    #education
+    #photo
+    #summary
+    #contacts
+    #skills
     pass
+
+
