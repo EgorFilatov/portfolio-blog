da4c1d77decbc153d9a617f9c62b7e630a0276d6
23egih23 <egor.filatov@live.com>
2022-02-11 16:13:27 +0300

    11.02

--- blog_site/main/views.py
+++ blog_site/main/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
+from .models import *
+from user.models import *
 
 # Create your views here.
+user = CustomUser.objects.get(first_name='Егор')
+user_first_name = user.first_name
+user_last_name = user.last_name
+about = user.about
+
+
 def home_page(request):
-    return render(request, "main/home_page.html", {})
+    return render(request, "main/home_page.html", {'first_name': user_first_name,
+                                                   'last_name': user_last_name,})
 
 def blog(request):
-    return render(request, "main/blog.html", {})
+    return render(request, "main/blog.html", {'first_name': user_first_name,
+                                              'last_name': user_last_name,
+                                              'about': about,})
