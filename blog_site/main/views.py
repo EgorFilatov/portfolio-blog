e67ddb3e870bbfbd2955e1f8bdd2bf62b0314570
23egih23 <egor.filatov@live.com>
2022-03-11 16:54:34 +0300

    11.03

--- blog_site/main/views.py
+++ blog_site/main/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -12,17 +13,21 @@ def home_page(request):
 
 
 class BlogNewsList(ListView):
+    paginate_by = 5
     model = News
     template_name = 'main/blog.html'
-    context_object_name = 'news'
+    context_object_name = 'a'
     extra_context = {}
 
 
 def category(request, cat_id):
     news = News.objects.filter(category_id=cat_id)
-    category = Categories.objects.get(pk=cat_id)
-    return render(request, "main/category.html", {'news': news,
-                                                  'category': category,})
+    category_name = Categories.objects.get(pk=cat_id)
+    paginator = Paginator(news, 5)
+    page_num = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_num)
+    return render(request, "main/category.html", {'page_obj': page_obj,
+                                                  'category_name': category_name,})
 
 
 class BlogNewsDetail(DetailView):
